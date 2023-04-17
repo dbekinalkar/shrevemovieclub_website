9703f1e3aab74113c60cab1563cc0c16af46ec58
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-04-17 17:03:48 -0400

    Partial Navbar

---
+++ lib/src/models/navbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Topbar extends StatelessWidget {
+  const Topbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar();
+  }
+}
